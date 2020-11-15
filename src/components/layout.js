7c9bd2ce9cd87db780a7a53b2cdb5fb366dfb3be
Rahul Ravella <ravella@berkeley.edu>
2020-11-15 15:10:27 -0600

    hi

--- src/components/layout.js
+++ src/components/layout.js
@@ -300,76 +300,7 @@ function Layout({ children }) {
 
             </section>
 
-            {/* Pricing cards */}
-            <section className="bg-gray-100 py-8">
-              
-              <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
-
-                <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Pricing</h1>
-                <div className="w-full mb-4">	
-                  <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-                </div>
-                
-                <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
-                  
-                  <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="p-8 text-3xl font-bold text-center border-b-4">Free</div>
-                      <ul className="w-full text-center text-sm">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                      </ul>
-                    </div>
-                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">£0 <span className="text-base">for one user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
-                    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="w-full p-8 text-3xl font-bold text-center">Basic</div>
-                      <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
-                      <ul className="w-full text-center text-base font-bold">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                      </ul>					
-                    </div>
-                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-4xl font-bold text-center">£x.99 <span className="text-base">/ per user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
-                    </div>
-                  </div>
-                  
-                  <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="p-8 text-3xl font-bold text-center border-b-4">Pro</div>
-                      <ul className="w-full text-center text-sm">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                      </ul>
-                    </div>
-                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">£x.99 <span className="text-base">/ per user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-                
-              </div>
-                
-            </section>
+           
 
             {/* Waves SVG */}
             <svg className="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -389,22 +320,7 @@ function Layout({ children }) {
               </g>
             </svg>
 
-            {/* CTA block */}
-            <section className="container mx-auto text-center py-6 mb-12">
-
-              <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">Call to Action</h1>
-              <div className="w-full mb-4">	
-                <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
-              </div>
-
-              <h3 className="my-4 text-3xl leading-tight">Read more about GatsbyJS</h3>	
-
-              <form action={data.site.siteMetadata.blog}>
-                <button type="submit"
-                        className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action!</button>
-              </form>
-
-            </section>
+            
 
             {/* Footer */}
             <footer className="bg-white">
